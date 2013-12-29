920af89c45c191d08d56fa14d994b71ee6754422
Ryan Lauck <rlauck@users.noreply.github.com>
2013-12-29 08:30:51 -0700

    dont really want to maintain this, a commented version can be added later once the code settles

--- index.js
+++
@@ -1,5 +0,0 @@
-r=function(s, f){
-	for((d=c.getContext('2d')).fillStyle=f, i=o%4/-2; i<19; i+=.08)
-		d.fillRect(s * (Q=Math.cos)(i) * i * 6 + 150, i * 16, z = s * Q(i-8) + 2, z)
-}
-setInterval("r(-1,'red',c.height=640),r(1,'#0f6',o++)", o=100)
